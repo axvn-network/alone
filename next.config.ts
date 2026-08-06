ce463392222c1202166ef0bf28c1c32e5ac2be22
Hoàng Bờm <namlangtu20162@gmail.com>
2026-08-07 02:13:51 +0700

    Dọn dẹp tệp rác, tối ưu hóa .gitignore và cấu hình nén Next.js siêu nhẹ khi Deploy

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: __dirname,
+  compress: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
   serverExternalPackages: ["cloudinary", "mongoose", "bcryptjs", "nodemailer"],
   images: {
+    formats: ["image/avif", "image/webp"],
     remotePatterns: [
       { protocol: "https", hostname: "res.cloudinary.com" },
     ],
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production" ? { exclude: ["error", "warn"] } : false,
   },
 };
 
